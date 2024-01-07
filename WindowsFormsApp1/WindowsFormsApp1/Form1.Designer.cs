73f04efeb5cccd75a6ad4a51607eb099a384eb92
Harshal Mandaliya <115624595+hmandaliya27@users.noreply.github.com>
2024-01-07 22:14:58 +0000

    Add files via upload

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,328 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.btnRight = new System.Windows.Forms.Button();
+            this.btnLeft = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.serialPort = new System.IO.Ports.SerialPort(this.components);
+            this.screenUpdate = new System.Windows.Forms.Timer(this.components);
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chartUpdate = new System.Windows.Forms.Timer(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tbLongitude = new System.Windows.Forms.TextBox();
+            this.tbLatitude = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.timerUpdate = new System.Windows.Forms.Timer(this.components);
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.progressBar2 = new System.Windows.Forms.ProgressBar();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.usbPortList = new System.Windows.Forms.ListBox();
+            this.Btnup = new System.Windows.Forms.Button();
+            this.Btndown = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOpen.Location = new System.Drawing.Point(22, 162);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(162, 25);
+            this.btnOpen.TabIndex = 10;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(423, 54);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(286, 266);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox3.TabIndex = 11;
+            this.pictureBox3.TabStop = false;
+            // 
+            // btnRight
+            // 
+            this.btnRight.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRight.Location = new System.Drawing.Point(736, 176);
+            this.btnRight.Name = "btnRight";
+            this.btnRight.Size = new System.Drawing.Size(69, 27);
+            this.btnRight.TabIndex = 9;
+            this.btnRight.Text = "Right";
+            this.btnRight.UseVisualStyleBackColor = true;
+            this.btnRight.Click += new System.EventHandler(this.btnRight_Click);
+            // 
+            // btnLeft
+            // 
+            this.btnLeft.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLeft.Location = new System.Drawing.Point(337, 176);
+            this.btnLeft.Name = "btnLeft";
+            this.btnLeft.Size = new System.Drawing.Size(71, 27);
+            this.btnLeft.TabIndex = 8;
+            this.btnLeft.Text = "Left";
+            this.btnLeft.UseVisualStyleBackColor = true;
+            this.btnLeft.Click += new System.EventHandler(this.btnLeft_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.Control;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(423, 54);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(286, 266);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(423, 54);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(286, 266);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // serialPort
+            // 
+            this.serialPort.PortName = "COM16";
+            // 
+            // screenUpdate
+            // 
+            this.screenUpdate.Enabled = true;
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(503, 375);
+            this.chart1.Name = "chart1";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            series2.XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Time;
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(415, 217);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // chartUpdate
+            // 
+            this.chartUpdate.Enabled = true;
+            this.chartUpdate.Interval = 5000;
+            this.chartUpdate.Tick += new System.EventHandler(this.chartUpdate_Tick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(22, 223);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 12;
+            this.textBox1.Text = "langitude";
+            // 
+            // tbLongitude
+            // 
+            this.tbLongitude.Location = new System.Drawing.Point(138, 249);
+            this.tbLongitude.Name = "tbLongitude";
+            this.tbLongitude.ReadOnly = true;
+            this.tbLongitude.Size = new System.Drawing.Size(100, 20);
+            this.tbLongitude.TabIndex = 13;
+            // 
+            // tbLatitude
+            // 
+            this.tbLatitude.Location = new System.Drawing.Point(138, 223);
+            this.tbLatitude.Name = "tbLatitude";
+            this.tbLatitude.ReadOnly = true;
+            this.tbLatitude.Size = new System.Drawing.Size(100, 20);
+            this.tbLatitude.TabIndex = 14;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox4.Location = new System.Drawing.Point(22, 249);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 15;
+            this.textBox4.Text = "longitude";
+            // 
+            // timerUpdate
+            // 
+            this.timerUpdate.Enabled = true;
+            this.timerUpdate.Interval = 1000;
+            this.timerUpdate.Tick += new System.EventHandler(this.timerUpdate_Tick);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(128, 455);
+            this.progressBar1.Maximum = 5000;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(131, 23);
+            this.progressBar1.TabIndex = 16;
+            this.progressBar1.Value = 5000;
+            // 
+            // progressBar2
+            // 
+            this.progressBar2.Location = new System.Drawing.Point(128, 489);
+            this.progressBar2.Maximum = 5000;
+            this.progressBar2.Name = "progressBar2";
+            this.progressBar2.Size = new System.Drawing.Size(131, 23);
+            this.progressBar2.TabIndex = 17;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox5.Location = new System.Drawing.Point(22, 455);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 18;
+            this.textBox5.Text = "fuel";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox6.Location = new System.Drawing.Point(22, 492);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 19;
+            this.textBox6.Text = "engine temperature";
+            // 
+            // usbPortList
+            // 
+            this.usbPortList.FormattingEnabled = true;
+            this.usbPortList.Location = new System.Drawing.Point(22, 24);
+            this.usbPortList.Name = "usbPortList";
+            this.usbPortList.Size = new System.Drawing.Size(162, 134);
+            this.usbPortList.TabIndex = 22;
+            // 
+            // Btnup
+            // 
+            this.Btnup.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btnup.Location = new System.Drawing.Point(521, 12);
+            this.Btnup.Name = "Btnup";
+            this.Btnup.Size = new System.Drawing.Size(71, 27);
+            this.Btnup.TabIndex = 23;
+            this.Btnup.Text = "Up";
+            this.Btnup.UseVisualStyleBackColor = true;
+            this.Btnup.Click += new System.EventHandler(this.Btnup_Click);
+            // 
+            // Btndown
+            // 
+            this.Btndown.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btndown.Location = new System.Drawing.Point(532, 326);
+            this.Btndown.Name = "Btndown";
+            this.Btndown.Size = new System.Drawing.Size(71, 27);
+            this.Btndown.TabIndex = 24;
+            this.Btndown.Text = "Down ";
+            this.Btndown.UseVisualStyleBackColor = true;
+            this.Btndown.Click += new System.EventHandler(this.Btndown_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkGray;
+            this.ClientSize = new System.Drawing.Size(947, 590);
+            this.Controls.Add(this.Btndown);
+            this.Controls.Add(this.Btnup);
+            this.Controls.Add(this.usbPortList);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.progressBar2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.tbLatitude);
+            this.Controls.Add(this.tbLongitude);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.btnRight);
+            this.Controls.Add(this.btnLeft);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Button btnRight;
+        private System.Windows.Forms.Button btnLeft;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.IO.Ports.SerialPort serialPort;
+        private System.Windows.Forms.Timer screenUpdate;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Timer chartUpdate;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox tbLongitude;
+        private System.Windows.Forms.TextBox tbLatitude;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Timer timerUpdate;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ProgressBar progressBar2;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.ListBox usbPortList;
+        private System.Windows.Forms.Button Btnup;
+        private System.Windows.Forms.Button Btndown;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        Bitmap rotatedBmp = null;
+        PointF pt = new PointF(0, 0);
+
+        Random rnd = new Random();
+        double dAltitude = 0.0;
+        DateTime dtTime;
+
+        String sSerialData = "";
+        float fHorizonAngleX = 0.0f;
+        float fHorizonAngleY = 0.0f;
+
+        // Model Data
+        int iRandomNumber = 0;
+        Random rndGenerator = new Random();
+
+        double dRadians = 0.0;
+        double dLat = 0.0;
+        double dLong = 0.0;
+
+        int angle = 0;
+        float pitch = 0;
+
+        // Create pen.
+        System.Drawing.Pen myPen = new System.Drawing.Pen(System.Drawing.Color.Black);
+
+
+        // Create points that define line.
+        Point point1 = new Point(0, 0);
+        Point point2 = new Point(0, 0);
+
+        public float FHorizonAngleY { get => fHorizonAngleY; set => fHorizonAngleY = value; }
+
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox2.Controls.Add(pictureBox3);
+            pictureBox3.Location = new Point(0, 0);
+            //pictureBox3.BackColor = Color.Transparent;
+            point1.X = pictureBox1.Width / 2;
+            point1.Y = pictureBox1.Height / 2;
+            point2 = point1;
+
+            // Draw line to screen. 
+            point1.X = point1.X - 10;
+            point2.X = point2.X + 10;
+
+        }
+
+        private void btnRight_Click(object sender, EventArgs e)
+        {
+            angle = angle + 5;
+
+            // Update Screen:
+            pictureBox2.Image = RotateImage(pictureBox1.Image, angle);
+        }
+
+        private void btnLeft_Click(object sender, EventArgs e)
+        {
+            angle = angle - 5;
+
+            pictureBox2.Image = RotateImage(pictureBox1.Image, angle);
+
+        }
+
+        public Bitmap RotateImage(Image image, PointF offset, float angle, float pitch = 0)
+        {
+            if (image == null)
+                throw new ArgumentNullException("image");
+
+            //create a new empty bitmap to hold rotated image
+            if (rotatedBmp == null)
+                rotatedBmp = new Bitmap(image.Width, image.Height);
+
+            rotatedBmp.SetResolution(image.HorizontalResolution, image.VerticalResolution);
+
+            //make a graphics object from the empty bitmap
+            Graphics g = Graphics.FromImage(rotatedBmp);
+
+
+
+            //Put the rotation point in the center of the image
+            g.TranslateTransform(offset.X, offset.Y);
+
+            //rotate the image
+            g.RotateTransform(angle);
+
+            g.TranslateTransform(-offset.X, -offset.Y);
+            g.TranslateTransform(0, pitch);
+
+
+
+            //draw passed in image onto graphics object
+            g.DrawImage(image, pt);
+
+
+
+
+            return rotatedBmp;
+        }
+
+        public Bitmap RotateImage(Image image, float angle, float pitch = 0)
+        {
+            return RotateImage(image, new PointF((float)image.Width / 2, (float)image.Height / 2), angle, pitch);
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            // 1 - Set Port Name
+            serialPort.PortName = "COM16";
+
+            // 2 - Open Port For Communication
+            serialPort.Open();
+
+        }
+
+        private void serialPort1_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            sSerialData = serialPort.ReadLine();
+            sSerialData = serialPort.ReadLine();
+
+            if (sSerialData.Contains("GyroX="))
+            {
+                sSerialData = sSerialData.Replace("GyroX=", "");
+                float.TryParse(sSerialData, out fHorizonAngleX);
+            }
+            else if (sSerialData.Contains("GyroY="))
+            {
+                sSerialData = sSerialData.Replace("GyroY=", "");
+                float.TryParse(sSerialData, out fHorizonAngleY);
+            }
+        }
+        private void screenUpdate_Tick(object sender, EventArgs e)
+        {
+            if (fHorizonAngleX > 0.0 || fHorizonAngleX < 0.0)
+                angle = -(int)fHorizonAngleX * 10;
+
+            if (fHorizonAngleY > 0.0 || fHorizonAngleY < 0.0)
+                pitch = (int)fHorizonAngleY * 50;
+        }
+
+        private void pictureBox3_Paint(object sender, PaintEventArgs e)
+        {
+
+            e.Graphics.DrawLine(myPen, point1, point2);
+        }
+        private void chartUpdate_Tick(object sender, EventArgs e)
+        {
+            dAltitude = rnd.Next(0, 100);
+            dtTime = DateTime.Now;
+            chart1.Series[0].Points.AddXY(dtTime, dAltitude);
+        }
+        private void timerUpdate_Tick(object sender, EventArgs e)
+        {
+            iRandomNumber = rndGenerator.Next(-180, 180);
+
+            dRadians += 0.01;
+            dLat = 180*Math.Sin(dRadians);
+            dLong = 180 * Math.Sin(-dRadians);
+
+            tbLatitude.Text = dLat.ToString();
+            tbLongitude.Text = dLong.ToString();
+            progressBar2.Increment(10);
+            progressBar1.Increment(-10);
+            LoadUSBPorts();
+            textBox1.Text = textBox1.Text + sSerialData + "\n";
+        }
+
+        private void button2_MouseEnter(object sender, EventArgs e)
+        {
+            Btndown.BackColor = Color.Red;
+        }
+
+        private void button2_MouseLeave(object sender, EventArgs e)
+        {
+            Btndown.BackColor = Color.Green;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadUSBPorts();
+        }
+        private void LoadUSBPorts()
+        {
+            usbPortList.Items.Clear();
+
+            // Get a list of serial port names.
+            string[] ports = System.IO.Ports.SerialPort.GetPortNames();
+
+            foreach (string port in ports)
+            {
+                usbPortList.Items.Add(port);
+            }
+        }
+
+       
+
+        private void Btnup_Click(object sender, EventArgs e)
+        {
+            pitch = pitch - 5;
+            pictureBox2.Image = RotateImage(pictureBox1.Image, angle, pitch);
+        }
+
+        private void Btndown_Click(object sender, EventArgs e)
+        {
+            pitch = pitch + 5;
+            pictureBox2.Image = RotateImage(pictureBox1.Image, angle, pitch);
+        }
+    }
+}
